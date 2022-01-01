d3ff5bb0544083776a9d7db0de13b4445859f1ca
StephenBuley <65041377+StephenBuley@users.noreply.github.com>
2022-01-01 11:52:43 +0100

    Setup graphql
    setup graphql, apolloServer and express, using v15.3.0 for graphql to closely follow the tutorial.

--- kireddit-server/src/index.ts
+++ kireddit-server/src/index.ts
@@ -1,15 +1,36 @@
 import { MikroORM } from "@mikro-orm/core"
-import { Post } from "./entities/Post"
 import mikroOrmConfig from "./mikro-orm.config"
+import express from "express"
+import { ApolloServer } from "apollo-server-express"
+import { buildSchema } from "type-graphql"
+import { HelloResolver } from "./resolvers/hello"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 const main = async () =>  {
     const orm = await MikroORM.init(mikroOrmConfig)
     await orm.getMigrator().up()
 
-    // const post = orm.em.create(Post, {title: "my first post"})
-    // await orm.em.persistAndFlush(post)
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
+    const app = express()
+
+    app.get('/', (_, res) => {
+        res.send("hello")
+    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
+    })
+
+    await apolloServer.start()
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("server started at localhost:4000")
+    })
 }
 
 main()
